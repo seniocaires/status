52e5047a8c13291ed804467e86f309c3eb4af48c
Senio Caires <seniocaires@gmail.com>
2017-10-05 14:50:29 -0300

    database H2

---
+++ src/main/resources/META-INF/carga.sql
@@ -0,0 +1,2 @@
+INSERT INTO SERVICOS("ID", "NOME", "LINK", "STATUS") VALUES (1, 'Google', 'https://www.google.com.br', 'LOADING')
+INSERT INTO SERVICOS("ID", "NOME", "LINK", "STATUS") VALUES (2, 'Senio Caires', 'https://seniocaires.com.br', 'LOADING')
