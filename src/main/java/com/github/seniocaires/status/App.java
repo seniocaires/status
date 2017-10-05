e9b24e34b8b67e6be0361b9a60a69f9b88987fa2
Senio Caires <seniocaires@gmail.com>
2017-10-05 08:15:03 -0300

    Root rest

---
+++ src/main/java/com/github/seniocaires/status/App.java
@@ -0,0 +1,8 @@
+package com.github.seniocaires.status;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class App extends Application {
+}
