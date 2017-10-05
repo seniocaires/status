eebe82c2356e524307ec2c15619d99ef25bf90b5
Senio Caires <seniocaires@gmail.com>
2017-10-05 08:15:30 -0300

    Rest serviços mock

---
+++ src/main/java/com/github/seniocaires/status/Resource.java
@@ -0,0 +1,20 @@
+package com.github.seniocaires.status;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+
+@Path("/status")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+public class Resource {
+
+  @GET
+  @Path("/")
+  public Response amostra() {
+    return Response.ok(new Gson().toJson(Util.servicosMock())).build();
+  }
+}
